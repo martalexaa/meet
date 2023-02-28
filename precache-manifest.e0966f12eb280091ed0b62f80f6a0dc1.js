c6dc50219485f21aa470bc8bbd440e7eac3f7dbc
martalexaa <63308375+martalexaa@users.noreply.github.com>
2023-02-28 09:44:49 +0100

    Updates

---
+++ precache-manifest.e0966f12eb280091ed0b62f80f6a0dc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "46c6a6b9c68cd90c8d77",
+    "url": "/meet/static/css/main.99f4a84d.chunk.css"
+  },
+  {
+    "revision": "46c6a6b9c68cd90c8d77",
+    "url": "/meet/static/js/main.46c6a6b9.chunk.js"
+  },
+  {
+    "revision": "3a79397ac1a1b3a66e79",
+    "url": "/meet/static/js/1.3a79397a.chunk.js"
+  },
+  {
+    "revision": "41007eed2637e67c6dfb",
+    "url": "/meet/static/js/2.41007eed.chunk.js"
+  },
+  {
+    "revision": "1920cbffc572bddfeef6",
+    "url": "/meet/static/js/runtime~main.1920cbff.js"
+  },
+  {
+    "revision": "f5fc6b7bb6a4fd95a7560d4ea508c109",
+    "url": "/meet/index.html"
+  }
+];
