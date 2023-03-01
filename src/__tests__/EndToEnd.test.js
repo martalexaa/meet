3e60eee5d6d2bd5ba8c9be8cf08332a386d2fffc
martalexaa <63308375+martalexaa@users.noreply.github.com>
2023-03-01 16:41:36 +0100

    User Acceptance & End-to-End Testing

---
+++ src/__tests__/EndToEnd.test.js
@@ -0,0 +1,40 @@
+import puppeteer from 'puppeteer';
+
+describe('show/hide an event details', () => {
+
+    let browser, page;
+    jest.setTimeout(30000);
+    beforeAll(async () => {
+        browser = await puppeteer.launch({
+            headless: false,
+            slowMo: 250, // slow actions by 250ms
+            ignoreDefaultArgs: ['--disable-extensions'] // ignores default setting that causes timeout errors
+        });
+        page = await browser.newPage();
+        await page.goto('http://localhost:3000/');
+        await page.waitForSelector('.Event');
+    });
+
+    test('An event element is collapsed by default', async () => {
+        const eventDetails = await page.$('.Event .details');
+        expect(eventDetails).toBeNull();
+    });
+
+    test('User can expand an event to see its details', async () => {
+        await page.click('.Event .details-btn');
+
+        const eventDetails = await page.$('.Event .details');
+        expect(eventDetails).toBeDefined();
+    });
+
+    test('User can collapse an event to hide its details', async () => {
+        await page.click('.Event .details-btn');
+        const eventDetails = await page.$('.Event .details');
+        expect(eventDetails).toBeNull();
+    });
+
+    afterAll(() => {
+        browser.close();
+    });
+
+});
