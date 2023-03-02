76063bb0bf065e8d97ce19207c130210e15481c6
martalexaa <63308375+martalexaa@users.noreply.github.com>
2023-03-02 11:46:17 +0100

    Set up Atatus

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('7209b10ab6924a96b180d44be0a20afd').install();
 
 ReactDOM.render(
   <React.StrictMode>
