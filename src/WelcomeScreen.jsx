6793f89e4290e4bce515888cbdfb70d3f58addbb
martalexaa <63308375+martalexaa@users.noreply.github.com>
2023-03-08 12:43:16 +0100

    css

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -5,7 +5,7 @@ function WelcomeScreen(props) {
     return props.showWelcomeScreen ?
         (
             <div className="WelcomeScreen">
-                <h1>Welcome to the Meet App</h1>
+                <h1 className="welcome-title" >Welcome to the Meet App</h1>
                 <h4>
                     Log in to see upcoming events around the world for
                     full-stack
